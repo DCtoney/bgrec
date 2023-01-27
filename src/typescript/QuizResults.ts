8525021113aa20ed7feb0d6fdeb125578b7de7d1
BrendanHoagie <Realbrendanhoag@gmail.com>
2023-01-27 15:10:12 -0500

    Added age quiz example, SASS for quiz homepage

---
+++ src/typescript/QuizResults.ts
@@ -0,0 +1,12 @@
+import Quiz from "./Quiz";
+
+// TODO: Finish making the results page display
+function displayQuizResults(quiz: Quiz): void{
+    let resultDiv = document.createElement("div")
+    resultDiv.classList.add("results-box")
+    
+    let quizResult = sessionStorage.getItem("result");
+
+    
+
+}
