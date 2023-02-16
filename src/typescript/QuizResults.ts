952a55076b96384b7acc2cc261d4bfd0f5b77489
BrendanHoagie <Realbrendanhoag@gmail.com>
2023-02-16 16:18:18 -0500

    Added quiz-results.html and ways to populate it

--- src/typescript/QuizResults.ts
+++ src/typescript/QuizResults.ts
@@ -1,12 +1,26 @@
 import Quiz from "./Quiz";
+import Game from "./Game" // For game result display
 
-// TODO: Finish making the results page display
 function displayQuizResults(quiz: Quiz): void{
     let resultDiv = document.createElement("div")
     resultDiv.classList.add("results-box")
     
-    let quizResult = sessionStorage.getItem("result");
+    let quizTitle = document.createElement("h3")
+    resultDiv.appendChild(quizTitle)
+    quizTitle.innerText = quiz.title
 
+    let quizResult = JSON.parse(sessionStorage.getItem("result")!);
+
+    let ageScore = document.createElement("H4")
+    resultDiv.appendChild(ageScore)
+    ageScore.innerHTML = `Your recommended age rage is ${quizResult.text}`
     
+    let results = document.createElement("h4")
+    resultDiv.appendChild(results)
+    results.innerText = quizResult.info
 
+    document.body.appendChild(resultDiv)
 }
+
+let theQuiz = JSON.parse(sessionStorage.getItem("quiz")!);
+displayQuizResults(theQuiz)
