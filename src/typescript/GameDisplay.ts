80809e26191cfc294c43f2f838659e22723cc913
ndi26 <ndi26@drexel.edu>
2023-03-13 10:56:58 -0400

    Unify design changes

--- src/typescript/GameDisplay.ts
+++ src/typescript/GameDisplay.ts
@@ -1,25 +1,6 @@
 import Game from "./Game";
 
 /**
- * This is a helper function to generate individual table elements for each entry in a list
- * 
- * **Parameters**
- * ```ts
- * let data: string[]
- * ```
- * - The list of data to be processed, mechanics/generes of a game
- * ```ts
- * let parent: HTMLElement
- * ```
- *  - this is the HTML table element which will contain all of the elements of the list
- */
-function populateFromList(data: string[], parent: HTMLElement) {
-	data.forEach(tag => {
-		parent.textContent += `${tag}, `;
-	});
-}
-
-/**
  * This populates the game result page with all of the game's properties as supplied by sessionStorage.
  * 
  * **Parameters**
@@ -33,13 +14,13 @@ function populateGame(game: Game) {
 	document.getElementById("title")!.innerHTML = `${game.name}`;
 	document.getElementById("description")!.innerHTML = `${game.longDescription}`;
 	document.getElementById("glo-rank")!.innerHTML = `${game.rank}`;
-	document.getElementById("user-rate")!.innerHTML = `${game.ratingAverage}/10, with ${game.usersRated} Ratings`;
+	document.getElementById("user-rate")!.innerHTML = `${game.ratingAverage}`;
 	document.getElementById("p-age")!.innerHTML = `${game.minimumAge}`;
 	document.getElementById("p-count")!.innerHTML = `${game.minimumPlayers} - ${game.maximumPlayers}`; // test if min = max
 	document.getElementById("estimated-time")!.innerHTML = `${game.minPlaytime} - ${game.maxPlaytime} mins`;
 	document.getElementById("year-pub")!.innerHTML = `${game.yearPublished}`;
-	populateFromList(game.genres, document.getElementById("genres")!);
-	populateFromList(game.mechanics, document.getElementById("mechanics")!);
+	document.getElementById("genres")!.innerHTML = `${game.genres.join(", ")}`;
+	document.getElementById("mechanics")!.innerHTML = `${game.mechanics.join(", ")}`;
 	document.getElementById("reference")!.setAttribute("href", `https://boardgamegeek.com/boardgame/${game.id}/${game.name}`);
 }
 
