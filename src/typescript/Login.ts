0e21e6479065ba78b74da2eb86e7394501edcc65
BrendanHoagie <Realbrendanhoag@gmail.com>
2023-03-14 03:28:33 -0400

    Added basic profile layout and content

--- src/typescript/Login.ts
+++ src/typescript/Login.ts
@@ -1,17 +1,20 @@
+
+
 $("#submit-button").on("click", _event => {
     console.log("button clicked!")
     if (document.querySelector<HTMLInputElement>('#uname')!.value && document.querySelector<HTMLInputElement>('#pass')!.value) {
         // I am aware we are storing a user password in plain text in session storage. 
         // I would use OAuth2.0 if this was a real project.
-        sessionStorage.setItem("username", document.getElementById('uname')!.innerText);
-        sessionStorage.setItem("password", document.getElementById('pass')!.innerText);
-        console.log("set session storage");
+        let username = (document.getElementById('uname') as HTMLInputElement).value;
+        let password = (document.getElementById('pass') as HTMLInputElement).value;
+        sessionStorage.setItem("username", username);
+        sessionStorage.setItem("password", password);
         window.location.href = "./profile.html";
     }
 });
 
+
 window.onload = () => {
-    console.log("window loads!");
     // instead of making a user that has already logged in do it again, redirect them to their account page
     if (sessionStorage.getItem("username") && sessionStorage.getItem("password")){
         window.location.href = "./profile.html";
